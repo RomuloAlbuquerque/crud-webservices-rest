146a1d9b7d042197a85d7c41ecea2bbbdb5463a1
Rômulo Albuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2021-10-01 14:35:43 -0300

    created method findById in the ClientService and ClientResource

--- src/main/java/com/projeto/service/ClientService.java
+++ src/main/java/com/projeto/service/ClientService.java
@@ -1,5 +1,7 @@
 package com.projeto.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -20,5 +22,13 @@ public class ClientService {
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> page = repository.findAll(pageRequest);
 		return page.map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional
+	public ClientDTO findById(Long id){
+		Optional<Client> objOptional = repository.findById(id);
+		Client entity = objOptional.get();
+		ClientDTO dto = new ClientDTO(entity);
+		return dto;
 	}
 }
